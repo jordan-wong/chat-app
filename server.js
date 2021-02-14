866f85ec99dd2586d8e8209009ab58bfdc294566
jordan-wong <61242306+jordan-wong@users.noreply.github.com>
2021-02-14 14:52:50 -0500

    login page / frontend structure additions

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const http = require('http')
+const socketio = require('socket.io');
+/*
+//const mongo = req
+var mongoose = require('mongoose');
+var dbUrl = 'mongodb+srv://jordan:mongoJ0r@cluster0.fsowp.mongodb.net/Cluster0?retryWrites=true&w=majorityongodb://wjor390:mongoJ0r@cluster0-shard-00-02.fsowp.mongodb.net:27017’
 
-var server = app.listen(3000, () => {
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongodb connected', err);
+})
+var Message = mongoose.model('Message', {name : String, message : String})
+*/
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
     console.log('server is running on port: ', server.address().port);
 })
-app.use(express.static(__dirname));
+
+//setting static folder
+app.use(express.static(__dirname+'/frontend'));
+
+//Client connection actions
+io.on('connection', socket =>{
+    console.log('a user has connected')
+    socket.emit('message', 'Welcome to the chat!');
+
+    //socket.broadcast to all OTHER clients
+    socket.broadcast.emit('message', 'A user has joined the chat!')
+
+    socket.on('disconnect', () =>{
+        io.emit('message', 'A user has left the chat')
+    });
+});
+//http.listen(3000, () => console.log("listening on 3000"))
